9b1947ba754a27d0c35a0b704c485c2021df28f4
minsongryu <ryums0712@archivsoft.com>
2020-10-05 14:09:55 +0900

    조회 화면 UI 및 기능 완성

--- components/board/BoardDetail.js
+++ components/board/BoardDetail.js
@@ -1,16 +1,38 @@
 import React, {Component} from 'react';
-import {SafeAreaView, ScrollView, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {SafeAreaView, ScrollView, Text, TextInput, TouchableOpacity, View, StyleSheet} from 'react-native';
 import main from '../../styles/main';
 import form from '../../styles/form';
+import axios from 'axios';
+import * as NavigationService from '../../utils/NavigationService';
 
 class BoardDetail extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            idx: this.props.route.params.idx,
             title: 'Default title',
             note: 'Default note'
         }
+    }
+
+    componentDidMount = () => {
+        axios.get('http://10.0.0.122:3000/boards/' + this.state.idx)
+            .then(res => {
+                if(res.status == 200 || res.status == 201) {
+                    const data = res.data[0]
+                    this.setState({
+                        title: data.title,
+                        note: data.note
+                    })
+                } else {
+                    alert('데이터 호출 오류')
+                }
+            })
+            .catch(err => {
+                alert(err)
+                NavigationService.goBack()
+            })
     }
 
     render() {
@@ -18,7 +40,7 @@ class BoardDetail extends Component {
             <SafeAreaView style={main.container}>
                 <View style={main.header}>
                     <Text style={main.title}>
-                        Detail of {this.props.route.params.idx}
+                        Detail of {this.state.idx}
                     </Text>
                 </View>
                 <ScrollView>
@@ -30,8 +52,9 @@ class BoardDetail extends Component {
                             <TextInput
                                 style={form.input}
                                 placeholder='제목을 입력해 주십시오.'
-                                // onChangeText={title => setTitle(title)}
-                                value={this.state.title}/>
+                                onChangeText={title => this.setState({title})}
+                                value={this.state.title}
+                            />
                         </View>
                         <View style={form.inputGroup}>
                             <View style={form.label}>
@@ -40,26 +63,56 @@ class BoardDetail extends Component {
                             <TextInput
                                 style={form.input}
                                 placeholder='내용을 입력해 주십시오.'
-                                // onChangeText={note => setNote(note)}
+                                onChangeText={note => this.setState({note})}
                                 value={this.state.note}
                             />
                         </View>
-                        {/*<TouchableOpacity*/}
-                        {/*    style={form.submit}*/}
-                        {/*    onPress={() => {*/}
-                        {/*        this.boardPut({*/}
-                        {/*            title: this.state.title,*/}
-                        {/*            note: this.state.note*/}
-                        {/*        })*/}
-                        {/*    }}*/}
-                        {/*>*/}
-                        {/*    <Text style={form.submitText}>등록</Text>*/}
-                        {/*</TouchableOpacity>*/}
                     </View>
                 </ScrollView>
+                <View style={styles.footer}>
+                    <TouchableOpacity
+                        style={[styles.button, styles.update]}
+                        onPress={() => {
+                            alert('준비 중...')
+                        }}
+                    >
+                        <Text style={form.submitText}>수정</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.button, styles.delete]}
+                        onPress={() => {
+                            alert('준비 중...')
+                        }}
+                    >
+                        <Text style={form.submitText}>삭제</Text>
+                    </TouchableOpacity>
+                </View>
             </SafeAreaView>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    footer: {
+        flexDirection: 'row',
+        padding: 10
+    },
+    button: {
+        width: '45%',
+        height: 50,
+        borderWidth: 0.5,
+        borderColor: 'black',
+        margin: 10,
+        borderRadius: 15,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    update: {
+        backgroundColor: 'blue'
+    },
+    delete: {
+        backgroundColor: 'red'
+    }
+})
 
 export default BoardDetail;
